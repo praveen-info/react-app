11391033bbd582ba367af07358761f1adb73821a
praveen-1999 <praveenm.emedia@gmail.com>
2021-06-28 22:47:27 +0530

    repo initialized

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
+import NavBar from './components/navbar';
+import Home from './components/home';
+import About from './components/about';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <NavBar/>
+        <div className="content">
+          <Route path="/" exact component={Home}/>
+          <Route path="/about" component={About}/>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
